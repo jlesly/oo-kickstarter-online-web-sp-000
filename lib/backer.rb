77c519e2f404cb43165a350f4f63f939c67daff0
Lesly Juarez <leslyj2@gmail.com>
2020-11-16 18:57:15 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_accessor :name, :backed_projects
+  
+  def initialize(name)
+    @name
+    @backed_projects []
+  end 
+  
+  def backed_project(project)
+    @backed_projects = backed_projects
+  end 
+  
+    
+  
